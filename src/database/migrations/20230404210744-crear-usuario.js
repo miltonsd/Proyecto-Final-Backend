4bba9f5842313360ada92592868bcedff22cbefc
Joaquin <jokinve1997@gmail.com>
2023-04-04 18:38:53 -0300

    Creado modelo de usuario

---
+++ src/database/migrations/20230404210744-crear-usuario.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+     await queryInterface.createTable('usuarios', { id_usuario: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    nombre: {
+      type: Sequelize.STRING(50),
+      allowNull: false
+    },
+    apellido: {
+      type: Sequelize.STRING(50),
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING(100),
+      allowNull: false
+    },
+    contraseña: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    documento: {
+      type: Sequelize.STRING(8),
+      allowNull: false
+    },
+    direccion: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    telefono: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    fechaNacimiento: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }});
+     
+  },
+
+  async down (queryInterface, Sequelize) {
+     await queryInterface.dropTable('usuarios');
+  
+  }
+};
---
+++ src/database/models/usuario.js
@@ -0,0 +1,90 @@
+'use strict'; 
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Usuario extends Model {
+     static associate(models) {
+      /* User.belongsToMany(models.Game, {through: 'Usergame', foreignKey: 'idUser'});
+        User.hasMany(models.Usergame, {foreignKey: 'idUser'});
+
+      User.belongsTo(models.Role, {foreignKey: 'idRole'}); */
+    } 
+  }
+  Usuario.init({ 
+    nombre: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      validate: {
+        isAlpha: { msg:"El nombre solo debe contener letras" },
+        len: {
+          args: [3,50],
+          msg: "El nombre debe contener entre 3 a 50 letras"
+        }
+      }
+    },
+    apellido: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      validate: {
+        isAlpha: { msg:"El apellido solo debe contener letras" },
+        len: {
+          args: [3,50],
+          msg: "El apellido debe contener entre 3 a 50 letras"
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+      validate: {
+        notNull: {msg:"Email requerido"},
+        isEmail: {msg:"Formato de email invalido"},
+        len: {
+          args: [5,100],
+          msg: "El correo puede contener hasta 100 caracteres maximo"
+        }
+      }
+    },
+    contraseña: {
+      type: DataTypes.STRING(150),
+      allowNull: false
+    },
+    id_rol: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 2 // Lo crea como usuario (1:AS|2:Usuario|3:Mozo)
+    },
+    documento: {
+        type: DataTypes.STRING(8),
+        allowNull: false,
+        unique: true,
+        validate: {
+            isNumeric: { msg:"El apellido solo debe contener numeros" },
+            len: {
+              args: [7,8],
+              msg: "El documento debe contener entre 7 y 8 numeros"
+            }
+          }
+      },
+      direccion: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      telefono: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isNumeric: { msg:"El telefono solo debe contener numeros" },
+      },
+      fechaNacimiento: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+  }, 
+    sequelize,
+    modelName: 'Usuario',
+  });
+  return Usuario;
+};
