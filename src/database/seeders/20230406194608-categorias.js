394c176349435c668e7695fc03c7272f54e46ee4
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2023-04-25 16:11:57 -0300

    Modificaciones en las semillas

--- src/database/seeders/20230406194608-categorias.js
+++ src/database/seeders/20230406194608-categorias.js
@@ -3,10 +3,10 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('categorias', [
-      { id: 1, descripcion: 'Cliente Básico', createdAt: new Date(), updatedAt: new Date() },
-      { id: 2, descripcion: 'Cliente GOLD', createdAt: new Date(), updatedAt: new Date() },
-      { id: 3, descripcion: 'Cliente PLATINUM', createdAt: new Date(), updatedAt: new Date() },
-      { id: 4, descripcion: 'Cliente BLACK', createdAt: new Date(), updatedAt: new Date() },
+      { cod_categoria: 1, descripcion: 'Cliente Básico', createdAt: new Date(), updatedAt: new Date() },
+      { cod_categoria: 2, descripcion: 'Cliente GOLD', createdAt: new Date(), updatedAt: new Date() },
+      { cod_categoria: 3, descripcion: 'Cliente PLATINUM', createdAt: new Date(), updatedAt: new Date() },
+      { cod_categoria: 4, descripcion: 'Cliente BLACK', createdAt: new Date(), updatedAt: new Date() },
     ], {});
   },
 
