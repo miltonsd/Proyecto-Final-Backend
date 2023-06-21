4fecc7f932f2ff6afc606a0a144480d7142a5f92
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2023-06-21 19:23:47 -0300

    Ordenadas las rutas en pedidos y ahora estan funcionado

--- src/routes/api/pedido.routes.js
+++ src/routes/api/pedido.routes.js
@@ -16,10 +16,10 @@ router.patch("/:id", updateOne(Pedido)); // Modifica un pedido
 
 // Rutas Especificas
 router.get("/", getAllPedidos); // Muestra todos
+router.get("/pendientes", getPendientes); // Muestra pendientes (Va antes que el GetOne porque sino toma 'pendientes' como si fuera un id o param)
 router.get("/:id", getOnePedido); // Muestra un pedido
 router.post("/create", createPedido); // Crea un pedido
-router.delete("/:id", deletePedido); // Elimina un pedido
-router.get("/pendientes", getPendientes); // Muestra pendientes
 router.post("/entregado/:id", setEntregado); // Entrega pedido marcandolo como isPendiente = false
+router.delete("/:id", deletePedido); // Elimina un pedido
 
 module.exports = router;
