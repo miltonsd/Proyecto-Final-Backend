85d2e7281ddce39383b0c0681de4b8e0c004f147
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2025-08-15 14:40:36 -0300

    Al borrar un tipoProducto, se asigna sus productos al Genérico (id=1). Además Genérico no se puede borrar.

--- src/controllers/models/tipoProducto.controller.js
+++ src/controllers/models/tipoProducto.controller.js
@@ -2,24 +2,28 @@ const { TipoProducto, Producto } = require('../../database/models/index');
 
 const deleteTipoProducto = async (req,res) => {
     try {
-        const id_tipoProducto = req.params.id;
-        const tipoProducto = await TipoProducto.findByPk(id_tipoProducto);
-        if (!tipoProducto) {
-            return res.status(404).json({msg:"Tipo producto no encontrado."})
-        } else {
-            // Busca todos los productos que pertenecen a ese tipo producto
-            // const productos = await Producto.findAll({
-            //     where: { id_tipoProducto: tipoProducto.id_tipoProducto } })
-            // console.log(productos)
-                // Asigna a todos los productos el tipo producto "Genérico" (ID: 0)
-            await Producto.update({ id_tipoProducto: 0 }, { where: { id_tipoProducto: tipoProducto.id_tipoProducto } })
-            //   ))
-            // Borro el tipo producto
-            tipoProducto.destroy();
-            return res.status(200).json({msg:"Tipo producto eliminado correctamente."})
+        const id_tipoProducto = parseInt(req.params.id, 10) // Se asegura que el param sea un número entero
+    
+        // Valida que no se quiera borrar el tipoProducto "Genérico" (id_tipoProducto = 1)
+        if (id_tipoProducto === 1 ) {
+            return res.status(403).json({ msg: "No se puede eliminar el tipo de producto 'Genérico'." })
         }
+
+        const tipoProducto = await TipoProducto.findByPk(id_tipoProducto)
+        
+        if (!tipoProducto) {
+            return res.status(404).json({ msg: "Tipo producto no encontrado." })
+        } 
+
+        // Actualiza todos los productos que contengan el tipoProducto a eliminar, asignándoles "Genérico"
+        await Producto.update({ id_tipoProducto: 1 }, { where: { id_tipoProducto: id_tipoProducto } })
+        
+        // Borro el tipo producto
+        await tipoProducto.destroy()
+
+        return res.status(200).json({ msg: "Tipo producto eliminado correctamente." })
     } catch (error) {
-        console.log(error);
+        console.error(error);
         res.status(500).json({ msg: 'Error en el servidor.' });
     }
 }
